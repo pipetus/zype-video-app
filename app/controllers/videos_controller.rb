970a67b90d5f8624fa4fde2abd1dfada9348486f
Juan Pablo Vercesi <jotapeve@gmail.com>
2019-09-19 14:18:36 -0300

    Rework model components and views

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,17 +1,6 @@
 class VideosController < ApplicationController
   def index
     @videos = Video.all
-    @videos.each do |video|
-      seconds = video[:duration] % 60
-      minutes = (video[:duration] / 60) % 60
-      hours = video[:duration] / (60 * 60)
-      video[:nice_duration] = if hours > 0
-                                format("%02d:%02d:%02d", hours, minutes, seconds)
-                              else
-                                format("%02d:%02d", minutes, seconds)
-                              end
-    end
-    @videos
   end
 
   def show
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,9 +1,33 @@
-class Video < ApplicationRecord
+class Video 
+  attr_reader :id, :title, :description, :nice_duration, :thumbnails
+
   def self.all
-    ::ZypeApi::Client.new.get_all
+    ::ZypeApi::Client.new.get_all.map do |attributes|
+      self.new(attributes)
+    end
   end
 
   def self.get(id)
-    ::ZypeApi::Client.new.get(id)
+    attributes = ::ZypeApi::Client.new.get(id)
+    self.new(attributes)
+  end
+
+  def initialize(options = {})
+    @title = options[:title]
+    @description = options[:description]
+    @id = options[:_id]
+    seconds, minutes, hours = if !options[:duration].nil?
+                                [options[:duration] % 60, 
+                                (options[:duration] / 60) % 60,
+                                options[:duration] / (60 * 60)]
+                              else
+                                [0, 0, 0]
+                              end
+    @nice_duration = if hours > 0
+                       format("%02d:%02d:%02d", hours, minutes, seconds)
+                     else
+                       format("%02d:%02d", minutes, seconds)
+                     end
+    @thumbnails = options[:thumbnails] || []
   end
 end
