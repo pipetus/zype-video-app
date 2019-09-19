00968948637429d3e0109657c57573452d20aeb4
Juan Pablo Vercesi <jotapeve@gmail.com>
2019-09-19 10:42:50 -0300

    Rework models

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,9 @@
 class Video < ApplicationRecord
   def self.all
-    ::ZypeApi::Client.new.get_all[:response]
+    ::ZypeApi::Client.new.get_all
+  end
+
+  def self.get(id)
+    ::ZypeApi::Client.new.get(id)
   end
 end
--- app/services/zype_api/client.rb
+++ app/services/zype_api/client.rb
@@ -9,20 +9,23 @@ module ZypeApi
       self.class.headers(default_headers.merge(options.fetch(:headers, {})))
     end
 
-    def get_all(options = { format: :plain })
-      response = self.class.get('', options.merge(query: { app_key: app_key }))
-      JSON.parse response, symbolize_names: true
+    def get_all
+      perform
     end
     
-    def get(id, options = {})
-      response = self.class.get("/#{id}", options)
-      JSON.parse response, symbolize_names: true
+    def get(id)
+      perform("/#{id}")
     end
-
-    private 
-
+    
     def app_key
       'XWny5j0V89yb1uZu6SI_D95EADV5FzBYldE9Ny0_q0GOzcWLiruPyhN-f2Pcyohf'
+    end
+    
+    private 
+
+    def perform(endpoint = '', options = { format: :plain })
+      response = self.class.get(endpoint, options.merge(query: { app_key: app_key }))
+      JSON.parse(response, symbolize_names: true)[:response]
     end
 
     def zype_api_base_uri
